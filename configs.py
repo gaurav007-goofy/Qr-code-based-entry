dd6a5359f741a3105a81913a4dde1b20c91b8ee3
Gautam Verma <gautam@blossomsocial.ca>
2022-12-09 11:22:42 +0530

    first commit

---
+++ configs.py
@@ -0,0 +1,16 @@
+class Formats:
+    timestamp_format = "%Y-%m-%d %H:%M:%S"
+
+
+class Keys:
+    first_name = "firstName"
+    last_name = "lastName"
+    college_id = "collegeId"
+    college_email_id = "collegeEmailId"
+    date_of_birth = "dateOfBirth"
+    phone_number = "phoneNumber"
+    student_id = "studentId"
+    updated_timestamp = "updatedTimestamp"
+    created_timestamp = "createdTimestamp"
+    password = "password"
+    image_url = "imageUrl"
--- temp.py
+++ temp.py
@@ -1,1 +1,17 @@
-asdfasdf
+import json
+
+import pyrebase
+pyrebase_config = json.load(open("serviceApiKey.json"))
+
+firebase = pyrebase.initialize_app(pyrebase_config)
+
+storage = firebase.storage()
+
+
+if __name__ == '__main__':
+    # file = open("images/195036.png")
+    # storage.child("display-pictures/195036.png").put("images/195036.png")
+
+    url = storage.child("display-pictures/195036.png").get_url(None)
+
+    print(url)
