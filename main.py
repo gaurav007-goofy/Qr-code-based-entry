a557904d7b5108884582bd7820d729032a7a6d2a
Gautam Verma <gautam@blossomsocial.ca>
2022-12-09 14:29:33 +0530

    changes

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from flask import render_template, Flask
 
 app = Flask(__name__)
 
+
 # app.static_folder = 'static'
 
 
 @app.route('/')
 def run():
     return render_template('homepage.html')
+
 
 
 if __name__ == "__main__":
