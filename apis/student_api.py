f4443a42e39123700d8b9687eefa17f70f097bd7
spikee <thegauravsaini007@gmail.com>
2022-12-10 23:47:46 +0530

    some changes

--- apis/student_api.py
+++ apis/student_api.py
@@ -1,5 +1,5 @@
 import json
-import pyrebase
+# import pyrebase
 import firebase_admin
 from firebase_admin import credentials, firestore
 from flask import Blueprint, request, render_template, Response
@@ -7,9 +7,9 @@ from configs import Keys
 import cv2
 from models.models import Student, EntryRequest
 
-pyrebase_config = json.load(open("serviceApiKey.json"))
-pyrebase_store = pyrebase.initialize_app(pyrebase_config)
-storage = pyrebase_store.storage()
+# pyrebase_config = json.load(open("serviceApiKey.json"))
+# pyrebase_store = pyrebase.initialize_app(pyrebase_config)
+# storage = pyrebase_store.storage()
 
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
